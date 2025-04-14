7af2f395f58950a3482c7ad410367c2cbf97bf8a
amanKumarBharti <183721382+Aman-bhart@users.noreply.github.com>
2025-04-14 17:01:53 +0530

    added syntax to add the important library for the project

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 chmod u+x ./hooks/pre-commit
 cp ./hooks/pre-commit .git/hooks
+deno add jsr:@std/assert
+deno add jsr:@std/testing
+
+echo "--------------------------repo initialised---------------------------"
