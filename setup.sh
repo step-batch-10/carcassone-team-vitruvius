95f616a694c29507b2034a7c04fafdfd6d71b82f
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-04-15 22:26:01 +0530

    modifies shell path

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#! /bin/bash
+
 chmod u+x ./hooks/pre-commit
 cp ./hooks/pre-commit .git/hooks
 deno init
 
-echo "--------------------------repo initialised---------------------------"
+echo "--------------------------repo initialized---------------------------"
