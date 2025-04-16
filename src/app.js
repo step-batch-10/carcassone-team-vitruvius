3ba3754f2516ba786286f2646e25f625fb9082e9
amanKumarBharti <183721382+Aman-bhart@users.noreply.github.com>
2025-04-16 13:53:07 +0530

    #

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/deno";
+import { getCookie } from "hono/cookie";
+
+const handleJoinReq = async (ctx) => {
+  const { roomId } = await ctx.parseBody();
+  const { sessionId } = getCookie(ctx);
+  const context = ctx.get("context");
+  const player = context.sessions.get(sessionId);
+
+  context.gameRoom.get(roomId).addPlayer(player);
+};
 
 const createHandler = () => {
   const app = new Hono();
 
   app.use(serveStatic({ root: "./public" }));
+  app.post("/joinRoom", handleJoinReq);
   app.post("/host", handle);
+
   return app;
 };
 
