66a27d9c01b8110b9adfba5c35eb394805155bd9
Inkeet C <inkeetcha234@gmail.com>
2025-04-16 16:58:49 +0530

    Changes setup.sh to add pre-push into .git/hooks

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 chmod u+x ./hooks/pre-commit
+chmod u+x ./hooks/pre-push
 cp ./hooks/pre-commit .git/hooks
+cp ./hooks/pre-push .git/hooks
 
 echo "--------------------------repo initialized---------------------------"
