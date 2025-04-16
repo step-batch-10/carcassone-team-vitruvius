973a9afe6629a8115bbfc4a6cb1b44f457735c43
amanKumarBharti <183721382+Aman-bhart@users.noreply.github.com>
2025-04-16 10:10:26 +0530

    added deno coverage condition

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@
 
 chmod u+x ./hooks/pre-commit
 cp ./hooks/pre-commit .git/hooks
-deno init
 
 echo "--------------------------repo initialized---------------------------"
--- test/demo_test.js
+++
@@ -1,8 +0,0 @@
-import { assert } from "@std/assert";
-import { describe, it } from "@std/testing/bdd";
-
-describe("it should pass", () => {
-  it("passing", () => {
-    assert(true);
-  });
-});
