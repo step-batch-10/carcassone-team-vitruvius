7b8b3495cfc5c4cbca17a1b01472de7452a9778c
mounika kolliboyina <183710046+mounikakolliboyina@users.noreply.github.com>
2025-04-16 23:04:59 +0530

    [#4] | resolves conflicts with game-lobby  | Mounika/Prasad

--- public/assets/script/join-host.js
+++ public/assets/script/join-host.js
@@ -1,12 +1,3 @@
-const hostRequest = (event) => {
-  event.preventDefault();
-  const formData = new FormData(event.target);
-  console.log(formData.get("noOfPlayers"));
-};
-
-function main() {
-  const form = document.querySelector("form");
-  form.addEventListener("submit", hostRequest);
-}
+function main() {}
 
 globalThis.addEventListener("DOMContentLoaded", main);
