3ef1e8cb88d3008f1610c5141e4e8748c6f60e0d
krishna <154574691+KrishnaDileepG@users.noreply.github.com>
2025-04-17 11:08:25 +0530

    [#38] | Adds a waiting page which displays all the players in the order they joined | Krishna/Meghashyam

--- public/assets/script/lobby.js
+++ public/assets/script/lobby.js
@@ -1,10 +1,63 @@
-const main = async() => {
-  // const gameDetails = document.querySelector("#game-details");
-  const roomID = document.querySelector("#room-id");
-  const response = await fetch("/room");
-  const {room} = await response.json();
-  roomID.textContent = `ROOM ID : ${room}`;
+const createRow = (index, { username, meepleColor }) => {
+  const row = document.createElement("tr");
+  const serial = document.createElement("td");
+  const name = document.createElement("td");
+  const color = document.createElement("td");
 
-}
+  serial.textContent = index;
+  name.textContent = username;
+  color.textContent = meepleColor;
+
+  row.append(serial, name, color);
+
+  return row;
+};
+
+const newTable = () => {
+  const playerDetails = document.querySelector("#players");
+  const table = document.createElement("table");
+  playerDetails.replaceChildren(table);
+
+  return table;
+};
+
+const fetchRoomData = async () => {
+  const roomData = await fetch("/room");
+
+  const { maxPlayers, roomID, players } = await roomData.json();
+
+  return { maxPlayers, roomID, players };
+};
+
+const appendRow = (table) => {
+  return (player, index) => {
+    const row = createRow(index + 1, player);
+
+    table.appendChild(row);
+  };
+};
+
+const startRoom = () => {
+  const intervalID = setInterval(async () => {
+    const roomIDDisplay = document.querySelector("#room-id");
+    const table = newTable();
+    const { maxPlayers, roomID, players } = await fetchRoomData();
+
+    players.forEach(appendRow(table));
+    roomIDDisplay.textContent = `ROOM ID : ${roomID}`;
+  }, 1000);
+};
+
+const main = () => {
+  startRoom();
+
+  const leave = document.querySelector("#leave-button");
+
+  leave.addEventListener("click", async (event) => {
+    await fetch("/leave", { method: "POST" });
+
+    globalThis.location = "/game-options";
+  });
+};
 
 globalThis.onload = main;
