1e9ce670cc5108cdbfed5ffb3496569a78965d44
Mohamad-sakib <158846349+Mohamad-sakib@users.noreply.github.com>
2025-04-17 10:59:31 +0530

    [#3] | adds logic for redirecting to join-game page once user click on joinBtn | sakib/aman

--- public/assets/script/join-host.js
+++ public/assets/script/join-host.js
@@ -1,9 +1,10 @@
 function main() {
   const joinBtn = document.getElementById("join-btn");
-  joinBtn.addEventListener(
-    "click",
-    () => (document.location = fetch("join-game.html"))
-  );
+  joinBtn.addEventListener("click", async () => {
+    const res = await fetch("html/join-game.html");
+
+    document.location.href = res.url;
+  });
 }
 
 globalThis.addEventListener("DOMContentLoaded", main);
