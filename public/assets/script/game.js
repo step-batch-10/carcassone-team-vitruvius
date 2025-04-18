f2fc27c55009ce30e5d879bbcd35bcb507d3718e
krishna <154574691+KrishnaDileepG@users.noreply.github.com>
2025-04-18 18:01:39 +0530

    [#37] | adds all unique tile images | Krishna/Meghashyam
    ## Description
    This fixes unused variables in game js
    
    ## Changes
    - Adds 24 images of all unique land tiles in carcassone

--- public/assets/script/game.js
+++ public/assets/script/game.js
@@ -1,7 +1,7 @@
-const tilePlacementListener = (cell) => {
+const tilePlacementListener = (cell, currentTilePath) => {
   const listener = (_) => {
     const img = document.createElement("img");
-    img.src = "/assets/images/tiles/rfrf-r.png";
+    img.src = currentTilePath;
 
     cell.appendChild(img);
 
@@ -13,10 +13,10 @@ const tilePlacementListener = (cell) => {
 
 let floatingImg;
 
-const tileMovements = (_) => {
+const tileMovements = (e, currentTilePath) => {
   if (!floatingImg) {
     floatingImg = document.createElement("img");
-    floatingImg.src = "/assets/images/tiles/rfrf-r.png";
+    floatingImg.src = currentTilePath;
     floatingImg.classList.add("tileMoves");
     document.body.appendChild(floatingImg);
   }
@@ -39,13 +39,15 @@ const showFloatingImage = (_) => {
 };
 
 const extractPath = (cell) => {
-  if (!cell) {
+  if (!cell.tile) {
     return null;
   }
-  const edges = cell.tileEdges.map((edge) => edge[0]).join("");
-  const center = cell.tileCenter[0];
 
-  return `/assets/images/tiles/${edges}-${center}.png`;
+  const edges = cell.tile.tileEdges.map((edge) => edge[0]).join("");
+  const center = cell.tile.tileCenter[0];
+  const guard = cell.tile.hasShield ? "-g" : "";
+
+  return `/assets/images/tiles/${edges}-${center}-${guard}.png`;
 };
 
 const setImage = (img, path, tiles, row, column, cell) => {
@@ -56,7 +58,7 @@ const setImage = (img, path, tiles, row, column, cell) => {
   }
 };
 
-const renderTiles = (gridSize, tiles, grid) => {
+const renderTiles = (gridSize, tiles, grid, currentTilePath) => {
   for (let row = 0; row < gridSize; row++) {
     for (let column = 0; column < gridSize; column++) {
       const cell = document.createElement("div");
@@ -65,31 +67,38 @@ const renderTiles = (gridSize, tiles, grid) => {
 
       setImage(img, path, tiles, row, column, cell);
 
-      cell.addEventListener("click", tilePlacementListener(cell));
+      cell.addEventListener(
+        "click",
+        tilePlacementListener(cell, currentTilePath)
+      );
       grid.appendChild(cell);
     }
   }
 };
 
-const addMouseListeners = (grid) => {
-  grid.addEventListener("mousemove", tileMovements);
+const addMouseListeners = (grid, currentTilePath) => {
+  grid.addEventListener("mousemove", (e) => tileMovements(e, currentTilePath));
   grid.addEventListener("mouseleave", hideFloatingImage);
   grid.addEventListener("mouseenter", showFloatingImage);
 };
 
 const main = async () => {
   const grid = document.getElementById("grid");
-  const gridSize = 84;
+  const gridSize = 3;
 
   grid.style.display = "grid";
   grid.style.gridTemplateColumns = `repeat(${gridSize}, 150px)`;
   grid.style.gridTemplateRows = `repeat(${gridSize}, 150px)`;
 
-  const res = await fetch("/game/board");
-  const tiles = await res.json();
+  const boardResponse = await fetch("/game/board");
+  const tiles = await boardResponse.json();
 
-  renderTiles(gridSize, tiles, grid);
-  addMouseListeners(grid);
+  const tileResponse = await fetch("/game/current-tile");
+  const currentTile = await tileResponse.json();
+  const currentTilePath = extractPath({ tile: currentTile });
+
+  renderTiles(gridSize, tiles, grid, currentTilePath);
+  addMouseListeners(grid, currentTilePath);
 };
 
 globalThis.onload = main;
