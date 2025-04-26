32833c7542312554e44f51512061f9a1f55ada5c
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-04-26 08:29:29 +0530

    Maped scroll navigators with arrow keys | Prasad/Sireesha

--- public/assets/script/scroll.js
+++ public/assets/script/scroll.js
@@ -7,61 +7,88 @@ const atBottomEdge = () =>
 const atRightEdge = () =>
   globalThis.scrollX + globalThis.innerWidth >= document.body.scrollWidth;
 
-const removeAlert = () => alertDiv.remove();
+const removeAlert = (alertPopUp) => alertPopUp.remove();
 
-const createAlert = (content) => {
-  const alertDiv = document.createElement("div");
-  alertDiv.classList.add("custom-alert");
-  alertDiv.textContent = content;
-  return alertDiv;
+const createPopUp = (content) => {
+  const popUp = document.createElement("div");
+
+  popUp.classList.add("custom-alert");
+  popUp.textContent = content;
+
+  return popUp;
 };
 
-const alertOnBoardEdge = (content) => {
-  createAlert(content);
-  document.body.append(alertDiv);
-  setTimeout(removeAlert, 1000);
+const showPopUpOnBoardEdge = (content) => {
+  const popUp = createPopUp(content);
+  document.body.append(popUp);
+
+  setTimeout(() => removeAlert(popUp), 1000);
 };
 
-const scroll = ({ predicate, direction, chord }) => {
-  const node = document.querySelector(`#${direction}-navigator`);
+const createScrollEvent = ({ predicate, direction, chord }) => () => {
+  if (!predicate()) {
+    return globalThis.scrollBy({ ...chord, behavior: "smooth" });
+  }
 
-  node.addEventListener("click", () => {
-    if (!predicate()) {
-      globalThis.scrollBy({
-        ...chord,
-        behavior: "smooth",
-      });
-    } else {
-      alertOnBoardEdge(`You are already in the ${direction} edge`);
-    }
+  showPopUpOnBoardEdge(`You are already in the ${direction} edge`);
+};
+
+const addScrollEvents = (scrollEventHandlers, navigators) => {
+  navigators.forEach((navigator, index) => {
+    navigator.addEventListener("click", scrollEventHandlers.at(index));
   });
 };
 
+const mapScrollEventsWithArrowKeys = (scrollEventHandlers, arrowKeys) => {
+  document.addEventListener("keydown", (event) => {
+    event.preventDefault();
+
+    arrowKeys.forEach((arrowKey, index) => {
+      if (event.key === arrowKey) scrollEventHandlers.at(index)();
+    });
+  });
+};
+
+const capitalize = (text) => text.at(0).toUpperCase().concat(text.slice(1));
+
 const addScrollFeatures = () => {
-  const directions = [
+  const scrollDirections = [
     {
       predicate: atRightEdge,
       direction: "right",
+      arrowKey: "right",
       chord: { left: 450 },
     },
     {
       predicate: atLeftEdge,
       direction: "left",
+      arrowKey: "left",
       chord: { left: -450 },
     },
     {
       predicate: atTopEdge,
       direction: "top",
+      arrowKey: "up",
       chord: { top: -450 },
     },
     {
       predicate: atBottomEdge,
       direction: "bottom",
+      arrowKey: "down",
       chord: { top: 450 },
     },
   ];
 
-  directions.forEach(scroll);
+  const scrollEventHandlers = scrollDirections.map(createScrollEvent);
+  const navigators = scrollDirections.map(({ direction }) =>
+    document.querySelector(`#${direction}-navigator`)
+  );
+  const arrowKeys = scrollDirections.map(({ arrowKey }) =>
+    "Arrow".concat(capitalize(arrowKey.toLowerCase()))
+  );
+
+  addScrollEvents(scrollEventHandlers, navigators);
+  mapScrollEventsWithArrowKeys(scrollEventHandlers, arrowKeys);
 };
 
 export default addScrollFeatures;
